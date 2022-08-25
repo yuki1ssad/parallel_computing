c552bfc22b076cf8ad162f5d64ee412c792d7457
yuki1ssad <18800205069@163.com>
2022-08-25 13:22:00 +0800

    data_mpi_4096

--- test_mpi.cpp
+++ test_mpi.cpp
@@ -6,16 +6,16 @@
 #include <omp.h>
 #include <fstream>
 
-void print_mat(int* M, int row, int col) {
-    std::cout << std::endl;
-    for (int i = 0; i <row; i++) {
-        for (int j = 0; j < col; j++) {
-            std::cout << M[i * col + j] <<" ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-}
+// void print_mat(int* M, int row, int col) {
+//     std::cout << std::endl;
+//     for (int i = 0; i <row; i++) {
+//         for (int j = 0; j < col; j++) {
+//             std::cout << M[i * col + j] <<" ";
+//         }
+//         std::cout << std::endl;
+//     }
+//     std::cout << std::endl;
+// }
 
 void Exp(Matrix &A, Matrix &B, int thrd, std::fstream &outfile) {
 
@@ -45,8 +45,8 @@ void Exp(Matrix &A, Matrix &B, int thrd, std::fstream &outfile) {
 
     // std::cout << "当前进程号：" << my_rank << std::endl;
 
-    int r_1_s = r_1;
-    if (r_1 % numprocs != 0) {
+    int r_1_s = r_1; // r_1_s保存A矩阵原始行数
+    if (r_1 % numprocs != 0) { // 若A原始行数不是进程数的倍数，则在最后添加元素全为0的行，使新行数是进程数的倍数
         r_1 -= r_1 % numprocs;
         r_1 += numprocs;
     }
@@ -54,13 +54,13 @@ void Exp(Matrix &A, Matrix &B, int thrd, std::fstream &outfile) {
     line = r_1 / numprocs; //每个进程处理的行数
     // std::cout << "line = " << line << std::endl;
 
-    mat_a = (int *)malloc(r_1 * c_1 * sizeof(int));
-    mat_b = (int *)malloc(r_2 * c_2 * sizeof(int));
-    buffer_a = (int *)malloc(line * c_1 * sizeof(int));
-    ans = (int *)malloc(line * c_2 * sizeof(int));
-    res = (int *)malloc(r_1 * c_2 * sizeof(int));
+    mat_a = (int *)malloc(r_1 * c_1 * sizeof(int)); // 为拉平后的A矩阵分配存储空间
+    mat_b = (int *)malloc(r_2 * c_2 * sizeof(int)); // 为拉平后的B矩阵分配存储空间
+    buffer_a = (int *)malloc(line * c_1 * sizeof(int)); // 为各进程分配到的部分A矩阵（line行）分配存储空间
+    ans = (int *)malloc(line * c_2 * sizeof(int)); // 为各进程计算所得结果分配存储空间
+    res = (int *)malloc(r_1 * c_2 * sizeof(int)); // res汇总各进程计算完毕发回的结果
 
-    if (mat_a == nullptr || buffer_a == nullptr || ans == nullptr || res == nullptr) {
+    if (mat_a == nullptr || buffer_a == nullptr || ans == nullptr || res == nullptr) { // 若分配内存失败，退出程序
         std::cout << "no enough memory" <<std::endl;
         exit(EXIT_FAILURE);
     }
@@ -71,7 +71,7 @@ void Exp(Matrix &A, Matrix &B, int thrd, std::fstream &outfile) {
             for (int j = 0; j < c_1; j++) {
                 if (i < r_1_s){
                     mat_a[i * c_1 + j] = A.get_element(i, j);
-                } else {
+                } else { //若有新增行，元素赋0值
                     mat_a[i * c_1 + j] = 0;
                 }  
             }
@@ -90,12 +90,12 @@ void Exp(Matrix &A, Matrix &B, int thrd, std::fstream &outfile) {
         start = MPI_Wtime();
     }
 
-    MPI_Scatter(mat_a, line * c_1, MPI_INT, buffer_a, line * c_1, MPI_INT, 0, MPI_COMM_WORLD); // 数据分发(mat_a)
+    MPI_Scatter(mat_a, line * c_1, MPI_INT, buffer_a, line * c_1, MPI_INT, 0, MPI_COMM_WORLD); // 0号进程执行数据分发(mat_a)
 
     // std::cout << "buffer_a:\n";
     // print_mat(buffer_a, line, c_1);
 
-    MPI_Bcast(mat_b, r_2 * c_2, MPI_INT, 0, MPI_COMM_WORLD); //数据广播(mat_b)
+    MPI_Bcast(mat_b, r_2 * c_2, MPI_INT, 0, MPI_COMM_WORLD); // 0号进程执行数据广播(mat_b)
 
     int tmp = 0;
     #pragma omp parallel for num_threads(thrd) // 对最外层for并行
@@ -142,7 +142,7 @@ int main(int argc, char* argv[]) {
     }
 
     int thrd = atoi(argv[1]); // thrd 接收传入的线程数
-    int scale = 2048;
+    int scale = 1024;
     Matrix A(scale, scale);
     Matrix B(scale, scale);
 
